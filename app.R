8f4a9b4ac5885dafece2cb63f973412d0134be8c
Raymond Li <rli3@andrew.cmu.edu>
2021-03-23 22:27:08 -0400

    added dragging marker feature

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(shinydashboard)
 library(leaflet)
 library(tidyverse)  
 library(here)
+library(leaflet.extras)
 
 
 #### Make a spatial data frame 
@@ -51,10 +52,9 @@ loadData <- function() {
   data
 }
 
-
-#df <- data.frame(matrix(NA, nrow=0, ncol=2))
-dataframe_click <- data.frame(matrix(NA, nrow=0, ncol=2))
-names(dataframe_click) <- c('Longitude', 'Latitude')
+# initializing dataframe
+dataframe_click <- data.frame(matrix(NA, nrow=0, ncol=3))
+names(dataframe_click) <- c('Longitude', 'Latitude', 'ShotId')
 
 #### Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -66,8 +66,10 @@ server <- function(input, output) {
     
     
     m = leaflet(df,width="100%",height="100%") %>% 
+      addDrawToolbar(circleOptions=NA, markerOptions=NA, polygonOptions=NA, 
+                     rectangleOptions=NA, polylineOptions=NA, circleMarkerOptions = NA) %>%
       addTiles()    %>%
-      addCircleMarkers() %>%
+      addCircleMarkers(options = markerOptions(draggable = TRUE)) %>%
       setView(lat = 40.47942168506459, lng=-79.85795114512402, zoom=17)
   })
   
@@ -79,10 +81,11 @@ server <- function(input, output) {
   
   #initialize a dataframe
   observeEvent(input$mymap_click, {
-    
-    #df = data.frame(name = input$mymap_click, used_shiny = input$used_shiny)
+
     click <- input$mymap_click #stores the data of each click, might have to populate a dataframe for every click
     text<-paste("Latitude ", round(click$lat,2), "Longtitude ", round(click$lng,2))
+    
+    shot_num <- nrow(dataframe_click) + 1
     
     proxy <- leafletProxy("mymap")
     
@@ -91,13 +94,28 @@ server <- function(input, output) {
       #clearGroup("new_point") %>% #remove this line
       #clearMarkers(layerId=input$mymap_click$id) %>%
       #addPopups(click$lng, click$lat) %>%  #try uncommenting this 
-      addCircles(click$lng, click$lat, radius=2, color="red", group = "new_point")
+      addCircleMarkers(click$lng, click$lat, radius=4, color="red", group = "new_point",
+                       layerId = shot_num, options = markerOptions(draggable = TRUE))
     
-    marker_long = click$lng
-    marker_lat = click$lat
+    # marker_long = click$lng
+    # marker_lat = click$lat
+    # print(click$layerId)
 
-    dataframe_click <<- dataframe_click %>% add_row(Longitude = click$lng, Latitude = click$lat)
+    dataframe_click <<- dataframe_click %>% add_row(Longitude = click$lng, Latitude = click$lat,
+                                                    ShotId = shot_num)
     saveData(dataframe_click)
+    
+    # nested observe event for dragging markers after initializing them
+    observeEvent(input$mymap_marker_dragend, {
+      drag <- input$mymap_marker_dragend
+      
+      # print(drag)
+      
+      dataframe_click$Longitude[dataframe_click$ShotId == drag$id] = drag$lng
+      dataframe_click$Latitude[dataframe_click$ShotId == drag$id] = drag$lat
+      
+      saveData(dataframe_click)
+    })
 
   })
   
@@ -105,9 +123,10 @@ server <- function(input, output) {
   observeEvent(input$clear, {
     leafletProxy("mymap") %>% 
       clearGroup("new_point")
+    # delete everything from dataframe
   })
   
-  print(nrow(dataframe_click))
+  # print(nrow(dataframe_click))
   #saveData(dataframe_click)
 
   
